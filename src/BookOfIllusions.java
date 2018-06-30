e6cb67025dbc6227285334ed9ea9ddb8e4e0f48d
Fangtastic7 <30578148+Fangtastic7@users.noreply.github.com>
2018-06-29 18:33:39 -0700

    New book + fortune

--- src/BookOfIllusions.java
+++ src/BookOfIllusions.java
@@ -10,47 +10,95 @@ import java.net.URL;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
- 
-/** We’re going to make a slideshow of cool optical illusions. When the user clicks on an illusion, a new one will be loaded. **/
+import javax.swing.JOptionPane;
+
+/**
+ * We’re going to make a slideshow of cool optical illusions. When the user
+ * clicks on an illusion, a new one will be loaded.
+ **/
 
 public class BookOfIllusions extends MouseAdapter {
 
 	/*
-	 * Here we are instantiating our BookOfIllusions class and calling it’s createBook() method. This is because we want to get out of the
-	 * static main method, so that we can add a click listener to each illusion.
+	 * Here we are instantiating our BookOfIllusions class and calling it’s
+	 * createBook() method. This is because we want to get out of the static main
+	 * method, so that we can add a click listener to each illusion.
 	 */
 
 	public static void main(String[] args) throws Exception {
 		BookOfIllusions illusions = new BookOfIllusions();
 		illusions.createBook();
 	}
-
+	int counter = 0;
 	// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+	JFrame frame = new JFrame();
+	
 	private void createBook() {
 		// 2. make the frame visible
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 3. set the size of the frame
+		frame.setSize(500, 200);
 		// 4. find 2 images and save them to your project’s default package
+
 		// 5. make a variable to hold the location of your image. e.g. "illusion.jpg"
+		String illusion1 = "illusion1.png";
+
 		// 6. create a variable of type "JLabel" but don’t initialize it yet
+		JLabel label = loadImageFromComputer(illusion1);
+
 		// 7. use the "loadImage..." methods below to initialize your JLabel
+
 		// 8. add your JLabel to the frame
+		frame.getContentPane().add(label);
+
 		// 9. call the pack() method on the frame
+		frame.pack();
 		// 10. add a mouse listener to your frame (hint: use *this*)
+		frame.addMouseListener(this);
 	}
 
 	public void mousePressed(MouseEvent e) {
+		counter = counter +1;
 		// 11. Print "clicked!" to the console when the mouse is pressed
+		System.out.println("Clicked!");
 		// 12. remove everything from the frame that was added earlier
+		frame.getContentPane().removeAll();
+		
 		// 13. load a new image like before (this is more than one line of code)
+		if(counter%3==0){
+			
+			String illusion1 = "illusion.png";
+			JLabel label = loadImageFromComputer(illusion1);
+			frame.getContentPane().add(label);
+			frame.getContentPane().add(label);
+		}
+		else if(counter%2==0) {
+			String illusion3 = "illusion3.jpg";
+			JLabel label3 = loadImageFromComputer(illusion3);
+			frame.getContentPane().add(label3);
+			
+		}
+		
 		// 14. pack the frame
+		else if(counter%1==0) {
+			String illusion2 = "illusion2.jpg";
+		
+		JLabel label2 = loadImageFromComputer(illusion2);
+		frame.getContentPane().add(label2);
+		}
+		frame.pack();
+		
 	}
 
 	// [OPTIONAL] 15. goad your users with some annoying or witty pop-ups
-
+		
 	/*
-	 * To use this method, the image must be placed in your Eclipse project under "default package".
+	 * To use this method, the image must be placed in your Eclipse project under
+	 * "default package".
 	 */
 	public JLabel loadImageFromComputer(String fileName) {
 		URL imageURL = getClass().getResource(fileName);
@@ -59,5 +107,3 @@ public class BookOfIllusions extends MouseAdapter {
 	}
 
 }
-
-
--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -10,9 +10,13 @@ import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.util.Random;
 
 import javax.imageio.ImageIO;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
@@ -23,20 +27,20 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
 
     JFrame frame = new JFrame();
 
-    int frameWidth = 500;
-    int frameHeight = 400;
-
+    int frameWidth = 250;
+    int frameHeight = 250;
+    private Clip clip;
     FortuneTeller() throws Exception {
    	 // 1. Choose an image for your fortune teller and put it in your default package
    	 fortuneTellerImage = ImageIO.read(getClass().getResource("fortune teller.png"));
    	 // 2. Adjust the frameWidth and frameHeight variables to fit your image nicely (doesn’t need a new line of code)
    	 // 4. add a mouse listener to the frame
-
+   	 frame.addMouseListener(this);
     }
 
     static void begin() {
    	 // 3. Welcome the user. Give them a hint for the secret location.
-
+    		JOptionPane.showMessageDialog(null, "Welcome, I will help consult your fortune's of the future! Please touch the center of the majestic ball!");
     }
 
     @Override
@@ -44,28 +48,41 @@ public class FortuneTeller extends JPanel implements Runnable, MouseListener {
    	 int mouseX = e.getX();
    	 int mouseY = e.getY();
    	 // 5. Print the mouseX variable
-
+   	 System.out.println(mouseX + ", " + mouseY);
    	 // 6. Add the mouseY variable to the previous line so that it prints out too (no new line)
    	 // 7. Adjust your secret location co-ordinates here:
-   	 int secretLocationX = 0;
-   	 int secretLocationY = 0;
+   	 int secretLocationX = 128;
+   	 int secretLocationY = 198;
    	 /** If the mouse co-ordinates and secret location are close, we'll let them ask a question. */
    	 if (areClose(mouseX, secretLocationX) && areClose(mouseY, secretLocationY)) {
    		 // 8. Get the user to enter a question for the fortune teller
-
+   		 String answer = JOptionPane.showInputDialog("Enter a question for me to consult for you.");
    		 // 9. Find a spooky sound and put it in your default package (freesound.org)
-   		 // AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
+   		  AudioClip sound = JApplet.newAudioClip(getClass().getResource("creepy-noise.wav"));
    		 // 10. Play the sound
-
+   		  playSound(sound);
    		 // 11. Use the pause() method below to wait until your music has finished
-
+   		  pause(4);
    		 // 12. Insert your completed Magic 8 ball recipe (http://bit.ly/Zdrf6d) here
-
+   		  
    	 }
 
     }
 
-    private boolean areClose(int mouseX, int secretLocationX) {
+    private void playSound(AudioClip sound) {
+		// TODO Auto-generated method stub
+    	try {
+			AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("creepy-noise.wav"));
+			 clip = AudioSystem.getClip();
+			clip.open(audioInputStream);
+			clip.start();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+	
+
+	private boolean areClose(int mouseX, int secretLocationX) {
    	 return mouseX < secretLocationX + 15 && mouseX > secretLocationX - 15;
     }
 
